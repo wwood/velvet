702a1a64bf7c5c1932f4e83a841dcf275813870b
Ben Woodcroft <donttrustben@gmail.com>
2013-08-01 22:21:37 +1000

    ye shall not destroy preNodes

--- src/concatenatedPreGraph.c
+++ src/concatenatedPreGraph.c
@@ -47,7 +47,7 @@ static void concatenatePreNodes(IDnum preNodeAID, PreArcI oldPreArc,
 		       hasSinglePreArc_pg(getOtherEnd_pg
 					  (preArc, preNodeBID),
 					  preGraph)
-		       && !isLoop_pg(preArc) 
+		       && !isLoop_pg(preArc)
 		       && getDestination_pg(preArc, preNodeBID) != preNodeAID) {
 
 		totalLength += getPreNodeLength_pg(preNodeBID, preGraph);
@@ -99,11 +99,11 @@ static void concatenatePreNodes(IDnum preNodeAID, PreArcI oldPreArc,
 		appendDescriptors_pg(&ptr, &writeOffset ,currentPreNodeID, preGraph, false);
 	}
 
-	if (writeOffset != 0) 
+	if (writeOffset != 0)
 		while (writeOffset++ != 4)
 			(*ptr) >>= 2;
 
-	setPreNodeDescriptor_pg(descr, totalLength - wordLength + 1, preNodeAID, preGraph); 
+	setPreNodeDescriptor_pg(descr, totalLength - wordLength + 1, preNodeAID, preGraph);
 
 	// Correct preArcs
 	for (preArc = getPreArc_pg(preNodeBID, preGraph); preArc != NULL_IDX;
@@ -209,7 +209,7 @@ static boolean isEligibleTip(IDnum index, PreGraph * preGraph, Coordinate
 	if (currentIndex == 0)
 		return true;
 
-	// Joined tips  
+	// Joined tips
 	if (simplePreArcCount_pg(-currentIndex, preGraph) < 2)
 		return false;
 
@@ -237,24 +237,24 @@ void clipTips_pg(PreGraph * preGraph)
 
 	velvetLog("Clipping short tips off preGraph\n");
 
-	while (modified) {
-		modified = false;
-		for (index = 1; index <= preNodeCount_pg(preGraph);
-		     index++) {
-			current = getPreNodeInPreGraph_pg(preGraph, index);
-
-			if (current == NULL)
-				continue;
-
-			if (isEligibleTip(index, preGraph, cutoffLength)
-			    || isEligibleTip(-index, preGraph,
-					     cutoffLength)) {
-				counter++;
-				destroyPreNode_pg(index, preGraph);
-				modified = true;
-			}
-		}
-	}
+//	while (modified) {
+//		modified = false;
+//		for (index = 1; index <= preNodeCount_pg(preGraph);
+//		     index++) {
+//			current = getPreNodeInPreGraph_pg(preGraph, index);
+//
+//			if (current == NULL)
+//				continue;
+//
+//			if (isEligibleTip(index, preGraph, cutoffLength)
+//			    || isEligibleTip(-index, preGraph,
+//					     cutoffLength)) {
+//				counter++;
+//				destroyPreNode_pg(index, preGraph);
+//				modified = true;
+//			}
+//		}
+//	}
 
 	concatenatePreGraph_pg(preGraph);
 	velvetLog("%li tips cut off\n", (long) counter);
